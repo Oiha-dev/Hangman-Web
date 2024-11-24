73245788b5139dfb6634c6df41b8059911ee9ce6
LASBENNES Lucas <lalucas@noreply.ytrack.learn.ynov.com>
2024-11-24 23:43:44 +0000

    fix(serverStart): Resolve merge conflict

--- internal/web/back/serverStart.go
+++ internal/web/back/serverStart.go
@@ -9,6 +9,8 @@ func StartServer() {
 	fmt.Println("(http://localhost:8080) - Server started on port 8080")
 	fmt.Println("Press Ctrl+C to stop the server")
 
+	http.HandleFunc("/", Index)
+	http.HandleFunc("/submit", LoginSubmit)
 	http.HandleFunc("/scoreboard", scoreboard)
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("internal/web/front"))))
 	http.Handle("/data/", http.StripPrefix("/data/", http.FileServer(http.Dir("data"))))
